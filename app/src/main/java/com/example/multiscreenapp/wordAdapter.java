ffdaf673cd35e15c2fd3fccc4037196f738b719a
Aditya Galande <adityagalande14@gmail.com>
2020-11-08 21:42:51 +0530

    wordAdapter.java

--- app/src/main/java/com/example/multiscreenapp/wordAdapter.java
+++ app/src/main/java/com/example/multiscreenapp/wordAdapter.java
@@ -1,4 +1,33 @@
 package com.example.multiscreenapp;
 
-public class wordAdapter {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class wordAdapter extends ArrayAdapter<word> {
+    public wordAdapter(Activity context, ArrayList<word> words) {
+        super(context, 0, words);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.custom_item_layout, parent, false);
+        }
+        word currentAndroidFlavor = getItem(position);
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.defaultTextView);
+        defaultTextView.setText(currentAndroidFlavor.getdefaultTranslation());
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwokTextView);
+        miwokTextView.setText(currentAndroidFlavor.getMiwokTranslationTranslation());
+
+        return listItemView;
+    }
 }
