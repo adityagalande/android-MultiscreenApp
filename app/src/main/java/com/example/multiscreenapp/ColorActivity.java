e51a66db45bfed4c5aecb8d33731143597439246
Aditya Galande <adityagalande14@gmail.com>
2020-11-10 14:12:00 +0530

    ColorActivity.java

--- app/src/main/java/com/example/multiscreenapp/ColorActivity.java
+++ app/src/main/java/com/example/multiscreenapp/ColorActivity.java
@@ -3,6 +3,9 @@ package com.example.multiscreenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class ColorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+        ArrayList<word> color = new ArrayList<word>();
+        color.add(new word("red","weṭeṭṭi"));
+        color.add(new word("green","chokokki"));
+        color.add(new word("brown","ṭakaakki"));
+        color.add(new word("gray","ṭopoppi"));
+        color.add(new word("black","kululli"));
+        color.add(new word("white","kelelli"));
+        color.add(new word("dusty yellow","ṭopiisә"));
+        color.add(new word("musterd yellow","chiwiiṭә"));
+
+        ListView listView = (ListView) findViewById(R.id.colorView);
+        wordAdapter colorArray = new wordAdapter(this, color);
+        listView.setAdapter(colorArray);
+
     }
 }
