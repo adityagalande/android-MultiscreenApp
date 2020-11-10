3d7f0e68002ccbeceb78001ff00c5d603e8a95c7
Aditya Galande <adityagalande14@gmail.com>
2020-11-10 14:12:24 +0530

    FamilyActivity.java

--- app/src/main/java/com/example/multiscreenapp/FamilyActivity.java
+++ app/src/main/java/com/example/multiscreenapp/FamilyActivity.java
@@ -3,6 +3,9 @@ package com.example.multiscreenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+        ArrayList<word> family = new ArrayList<word>();
+
+        family.add(new word("father", "әpә"));
+        family.add(new word("mother", "әṭa"));
+        family.add(new word("son", "angsi"));
+        family.add(new word("daughter", "tune"));
+        family.add(new word("older brother", "taachi"));
+        family.add(new word("younger brother", "chalitti"));
+        family.add(new word("older sister", "teṭe"));
+        family.add(new word("younger sister", "kolliti"));
+        family.add(new word("grandmother", "ama"));
+        family.add(new word("grandfather", "paapa"));
+
+        ListView listView = (ListView) findViewById(R.id.familyView);
+        wordAdapter familyArray = new wordAdapter(this,family);
+        listView.setAdapter(familyArray);
+
     }
 }
