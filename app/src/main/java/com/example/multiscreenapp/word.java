677c55695804b50a9537510998ba7a8ff6e9f254
Aditya Galande <adityagalande14@gmail.com>
2020-11-13 02:25:55 +0530

    word.java

--- app/src/main/java/com/example/multiscreenapp/word.java
+++ app/src/main/java/com/example/multiscreenapp/word.java
@@ -1,19 +1,30 @@
 package com.example.multiscreenapp;
 
+import android.graphics.drawable.Drawable;
+
 public class word {
     private String defaultTranslation;
     private String miwokTranslation;
+    private int imageID;
 
     public word(String defaultTranslation, String miwokTranslation) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
     }
 
-    public String getdefaultTranslation() {
+    public word(String defaultTranslation, String miwokTranslation, int imageID) {
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+        this.imageID = imageID;
+    }
+
+    public String getDefaultTranslation() {
         return this.defaultTranslation;
     }
 
     public String getMiwokTranslationTranslation() {
         return this.miwokTranslation;
     }
+
+    public int getImageID() { return this.imageID; }
 }
