1198dace1463281decb750a7e0b8164f22a07d17
Aditya Galande <adityagalande14@gmail.com>
2020-11-13 02:29:12 +0530

    FamilyActivity.java

--- app/src/main/java/com/example/multiscreenapp/FamilyActivity.java
+++ app/src/main/java/com/example/multiscreenapp/FamilyActivity.java
@@ -15,16 +15,16 @@ public class FamilyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_family);
         ArrayList<word> family = new ArrayList<word>();
 
-        family.add(new word("father", "әpә"));
-        family.add(new word("mother", "әṭa"));
-        family.add(new word("son", "angsi"));
-        family.add(new word("daughter", "tune"));
-        family.add(new word("older brother", "taachi"));
-        family.add(new word("younger brother", "chalitti"));
-        family.add(new word("older sister", "teṭe"));
-        family.add(new word("younger sister", "kolliti"));
-        family.add(new word("grandmother", "ama"));
-        family.add(new word("grandfather", "paapa"));
+        family.add(new word("father", "әpә",R.drawable.family_father));
+        family.add(new word("mother", "әṭa",R.drawable.family_mother));
+        family.add(new word("son", "angsi",R.drawable.family_son));
+        family.add(new word("daughter", "tune",R.drawable.family_daughter));
+        family.add(new word("older brother", "taachi",R.drawable.family_older_brother));
+        family.add(new word("younger brother", "chalitti",R.drawable.family_younger_brother));
+        family.add(new word("older sister", "teṭe",R.drawable.family_older_sister));
+        family.add(new word("younger sister", "kolliti",R.drawable.family_younger_sister));
+        family.add(new word("grandmother", "ama",R.drawable.family_grandmother));
+        family.add(new word("grandfather", "paapa",R.drawable.family_grandfather));
 
         ListView listView = (ListView) findViewById(R.id.familyView);
         wordAdapter familyArray = new wordAdapter(this,family);
