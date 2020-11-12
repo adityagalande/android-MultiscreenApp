5adabdbe9022afb52c53353e06bf9f9ccaffb64f
Aditya Galande <adityagalande14@gmail.com>
2020-11-13 02:27:37 +0530

    ColorActivity.java

--- app/src/main/java/com/example/multiscreenapp/ColorActivity.java
+++ app/src/main/java/com/example/multiscreenapp/ColorActivity.java
@@ -14,14 +14,14 @@ public class ColorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
         ArrayList<word> color = new ArrayList<word>();
-        color.add(new word("red","weṭeṭṭi"));
-        color.add(new word("green","chokokki"));
-        color.add(new word("brown","ṭakaakki"));
-        color.add(new word("gray","ṭopoppi"));
-        color.add(new word("black","kululli"));
-        color.add(new word("white","kelelli"));
-        color.add(new word("dusty yellow","ṭopiisә"));
-        color.add(new word("musterd yellow","chiwiiṭә"));
+        color.add(new word("red","weṭeṭṭi",R.drawable.color_red));
+        color.add(new word("green","chokokki",R.drawable.color_green));
+        color.add(new word("brown","ṭakaakki",R.drawable.color_brown));
+        color.add(new word("gray","ṭopoppi",R.drawable.color_gray));
+        color.add(new word("black","kululli",R.drawable.color_black));
+        color.add(new word("white","kelelli",R.drawable.color_white));
+        color.add(new word("dusty yellow","ṭopiisә",R.drawable.color_dusty_yellow));
+        color.add(new word("mustard yellow","chiwiiṭә",R.drawable.color_mustard_yellow));
 
         ListView listView = (ListView) findViewById(R.id.colorView);
         wordAdapter colorArray = new wordAdapter(this, color);
